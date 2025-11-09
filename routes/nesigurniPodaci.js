c57cd715130f7d38c64b0ffdda60dfb05d7dccc8
Kristina Đunđek <krdund@taltech.ee>
2025-11-09 23:09:26 +0100

    Update nesigurniPodaci.js

--- routes/nesigurniPodaci.js
+++ routes/nesigurniPodaci.js
@@ -29,14 +29,14 @@ router.post('/', async (req, res) => {
         return res.status(400).send('Nedostaje ime, prezime, email ili lozinka korisnika.');
     }
     if (ranjivost) {
-        const formData = {eHash: email, encryptedData: ime+prezime+email, hashedPass: lozinka};
+        const formData = {eHash: email, encryptedData: ime+"&"+prezime+"&"+email, hashedPass: lozinka};
         req.session.formData = formData;
     } else {
         const hashedPass = await bcrypt.hash(lozinka, saltRounds);
         hash.update(email);
         const eHash = hash.digest('hex');
         const key = crypto.randomBytes(32); // Generate a secure key
-        const encryptedData = encrypt(ime+prezime+email, key);
+        const encryptedData = encrypt(ime+"&"+prezime+"&"+email, key);
         const formData = {eHash: eHash, encryptedData: encryptedData, hashedPass: hashedPass};
         req.session.formData = formData;
     }
